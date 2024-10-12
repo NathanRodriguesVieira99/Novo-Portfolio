70306538748ac4613beda41b3ab1e475c6ca0aae
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2024-10-12 18:07:40 -0300

    urls redes agora acessiveis

--- novo-port/src/components/SocialNetworks/SocialNetworks.jsx
+++ novo-port/src/components/SocialNetworks/SocialNetworks.jsx
@@ -2,16 +2,16 @@ import { FaLinkedinIn, FaGithub, } from 'react-icons/fa';
 import '../../styles/components/SocialNetworks/SocialNetworks.scss';
 
 const socialMedias = [
-  { name: 'linkedin', icon: <FaLinkedinIn /> },
-  { name: 'github', icon: <FaGithub /> }
+  { name: 'linkedin', icon: <FaLinkedinIn />, url: 'https://www.linkedin.com/in/nathan-rodrigues-vieira-207747252/' },
+  { name: 'github', icon: <FaGithub />, url: 'https://github.com/NathanRodriguesVieira99' }
 ];
 
 function SocialNetworks() {
   return (
     <>
-      <section id="Social-Networks"> 
+      <section id="Social-Networks">
         {socialMedias.map((network) => (
-          <a href="." className="social-btn" id={network.name} key={network.name}>
+          <a href={network.url} target='_blank' className="social-btn" id={network.name} key={network.name}>
             {network.icon}
           </a>
         ))}
